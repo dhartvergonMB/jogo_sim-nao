1c0f7c558e7737e8971e478e7640875f8d57a461
matheus <matheusbar750@gmail.com>
2022-02-17 17:41:25 -0300

    upando o projeto

---
+++ script.js
@@ -0,0 +1,36 @@
+let buttonNo = document.querySelector('#buttonNo');
+let tentativas = 0;
+buttonNo.onmouseover = trocarLugar;
+buttonNo.onclick = parabens;
+
+let buttonYes = document.querySelector('#buttonYes');
+buttonYes.onclick = alegrarSe;
+
+function trocarLugar(){
+    tentativas++;
+    buttonNo.style.left = Math.floor(Math.random() * 75) + '%';
+    buttonNo.style.top = Math.floor(Math.random() * 90) + '%';
+    console.log(buttonNo.style.left);
+    console.log(buttonNo.style.top);
+}
+
+function alegrarSe(){
+    window.location.href = 'https://www.kayak.com.br/news/lua-de-mel-no-brasil/';
+}
+
+function parabens(){
+    alert("Parabens, depois de "+ tentativas+" tentativas descobrimos que você realmente não gosta dele(a)");
+}
+
+
+
+
+
+
+
+
+
+
+
+const array = ['maçã', 'banana', 'abacate', 'acerola', 'manga'];
+console.log(array.includes('acerola'));
